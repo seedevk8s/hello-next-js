f9981cb4d553277ac33a700df5aa53f8ff07a4e5
unknown <D191375@HYUNDAI-GLOVIS.COM>
2019-08-21 16:25:53 +0900

    페이지 playground.js 추가

---
+++ .next/static/development/pages/playground.js
@@ -0,0 +1,99 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\playground.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fplayground&absolutePagePath=C%3A%5Creact-tutorial%5Chello-next-js%5Cpages%5Cplayground.js!./":
+/*!************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fplayground&absolutePagePath=C%3A%5Creact-tutorial%5Chello-next-js%5Cpages%5Cplayground.js ***!
+  \************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/playground", function() {
+      var page = __webpack_require__(/*! ./pages/playground.js */ "./pages/playground.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/playground.js */ "./pages/playground.js", function() {
+          if(!next.router.components["/playground"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/playground.js */ "./pages/playground.js")
+          next.router.update("/playground", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_01f9a3fa864a7b7414d8 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_01f9a3fa864a7b7414d8 */ "dll-reference dll_01f9a3fa864a7b7414d8"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/playground.js":
+/*!*****************************!*\
+  !*** ./pages/playground.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\react-tutorial\\hello-next-js\\pages\\playground.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Playground"), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Welcome to playground."));
+});
+
+/***/ }),
+
+/***/ 2:
+/*!****************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fplayground&absolutePagePath=C%3A%5Creact-tutorial%5Chello-next-js%5Cpages%5Cplayground.js ***!
+  \****************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fplayground&absolutePagePath=C%3A%5Creact-tutorial%5Chello-next-js%5Cpages%5Cplayground.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fplayground&absolutePagePath=C%3A%5Creact-tutorial%5Chello-next-js%5Cpages%5Cplayground.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_01f9a3fa864a7b7414d8":
+/*!*******************************************!*\
+  !*** external "dll_01f9a3fa864a7b7414d8" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_01f9a3fa864a7b7414d8;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=playground.js.map
